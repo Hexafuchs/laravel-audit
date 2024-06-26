1b7b6e295e94acabd615faeff1afcafb4c2691fa
Leo <leo@hexafuchs.de>
2024-06-26 13:25:01 +0200

    :white_check_mark: Add tests

---
+++ tests/ConfigTest.php
@@ -0,0 +1,50 @@
+<?php
+
+it('contains only strings in check config', function () {
+    foreach (config('audit.checks') as $check) {
+        expect($check)->toBeString();
+    }
+});
+
+it('contains only checks with execute functions', function () {
+    foreach (config('audit.checks') as $check) {
+        $instance = app()->make($check);
+        expect(get_class($instance))
+            ->toHaveMethod('execute');
+    }
+});
+
+it('contains checks with different names', function () {
+    foreach (config('audit.checks') as $check) {
+        $instance = app()->make($check);
+        expect($instance->execute())->toBeInstanceOf(\Hexafuchs\Audit\Checks\CheckResult::class);
+    }
+});
+
+it('contains no null values', function () {
+    foreach (config('audit.checks') as $check) {
+        $instance = app()->make($check);
+        expect($instance->getName())->not->toBeEmpty()
+            ->and($instance->getGroup())->not->toBeEmpty()
+            ->and($instance->getError())->not->toBeEmpty();
+    }
+});
+
+it('contains different names', function () {
+    $names = [];
+    foreach (config('audit.checks') as $check) {
+        $instance = app()->make($check);
+        $names[] = $instance->getName();
+    }
+    expect(sizeof($names))->toBe(sizeof(array_unique($names)));
+});
+
+it('contains different errors', function () {
+    $errors = [];
+    foreach (config('audit.checks') as $check) {
+        $instance = app()->make($check);
+        $errors[] = $instance->getError()->message;
+    }
+
+    expect(sizeof($errors))->toBe(sizeof(array_unique($errors)));
+});
---
+++ tests/ResultTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Hexafuchs\Audit\Facades\Audit;
+
+it('returns check results', function () {
+    $result = Audit::run();
+
+    expect($result->doesntContain(fn ($e) => !($e instanceof \Hexafuchs\Audit\Checks\CheckResult)))->toBeTrue();
+});
+
+it('contains enough results', function () {
+    $result = Audit::run();
+
+    expect($result->count() === sizeof(config('audit.checks')))->toBeTrue();
+});
+
+it('contains no null group names', function () {
+    $result = Audit::run();
+
+    expect($result->map(fn ($e) => $e->group)->doesntContain(fn ($e) => $e === null))->toBeTrue();
+});
+
+
+it('contains unique names', function () {
+    $result = Audit::run();
+    $names = $result->map(fn ($e) => $e->name);
+
+    expect($names->count() === $names->unique()->count())->toBeTrue();
+});
+
+
